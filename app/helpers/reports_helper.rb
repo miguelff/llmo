fea8efeb648e981b04ec3aaf790616e82ad25101
Miguel Fernandez <miguel@mff.io>
2024-11-13 12:09:13 +0100

    Add more form fields

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,25 @@
 module ReportsHelper
+  def region_options
+    [
+        [
+            "Any",
+            [
+                [ "🗺️ Any region", "any" ]
+            ]
+        ],
+        [
+        "Regions",
+            [
+                [ "🌎 North America (NA)", "north america" ],
+                [ "🌍 Europe, Middle East & Africa (EMEA)", "europe, middle east & africa" ],
+                [ "🌏 Asia Pacific (APAC)", "asia pacific" ],
+                [ "🌎 Latin America (LATAM)", "latin america" ]
+            ]
+        ],
+        [
+                "Countries",
+                ISO3166::Country.all.map { |c| [ "#{c.emoji_flag} #{c.iso_short_name}", c.iso_short_name ] }
+        ]
+    ]
+  end
 end
