168c30529589b8635d04d792987e2815b5147b57
Miguel Fernandez <miguel@mff.io>
2024-11-19 13:48:03 +0100

    Change migrations

--- db/migrate/20241116200428_add_devise_to_users.rb
+++ db/migrate/20241116200428_add_devise_to_users.rb
@@ -2,7 +2,8 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[8.0]
   def self.up
-    # TODO: make sure to change the email to be unique
+    drop_table :users
+
     change_table :users do |t|
       ## Database authenticatable
       t.string :encrypted_password, null: false, default: ""
@@ -37,6 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[8.0]
       # t.timestamps null: false
     end
 
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
