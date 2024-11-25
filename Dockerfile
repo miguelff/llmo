1d197e650cada9823b0ac934af189e58405386a9
Miguel Fernandez <miguel@mff.io>
2024-11-25 18:10:54 +0100

    Expose topic classification for reuse

--- Dockerfile
+++ Dockerfile
@@ -49,7 +49,7 @@ COPY . .
 # Precompile bootsnap code for faster boot times
 RUN bundle exec bootsnap precompile app/ lib/
 
-RUN cd vendor/llmo && npm install && npm run build
+RUN cd vendor/llmo && npm install && npm run build && npm install flowbite
 
 # Precompiling assets for production without requiring secret RAILS_MASTER_KEY
 RUN SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
--- app/models/analysis/topic_classification.rb
+++ app/models/analysis/topic_classification.rb
@@ -1,22 +1,11 @@
 class Analysis::TopicClassification < ApplicationRecord
-    include Analysis::InferenceStep
-
-    belongs_to :report
-
-    schema do
-        string :entity_type, enum: %w[brand product service other], description: "The detected category for the input text"
-        string :brand, required: false, description: "If it's a brand, the detected brand for the input text, if it's a product, the brand of or maker of the product"
-        string :product, required: false, description: "If it's a product, the detected product for the input text, minus the brand, that must be included in the brand field"
-        string :other, required: false, description: "If it's not a brand or product, the detected category for the input text"
-    end
-
-    system <<-EOF.squish
+    SYSTEM_PROMPT = <<-EOF.squish
         You are an assistant specialized in entity recognition.
         Your task is to analyze text and determine the category of the entity in the text.
         This is a list of entities we support:
             * brand: a brand name, like 'Apple', 'Samsung', 'Google', etc.
             * product: a product name, like 'iPhone', 'Galaxy', 'Pixel', etc.
-            * service: A provided or subscribed offering, such as “Netflix,” “Amazon Prime,” or “Uber.”
+            * service: A provided or subscribed offering, such as 'Netflix', 'Amazon Prime' or 'Uber'.
 
         If the entity includes both the brand and the product, return 'product' and set the brand accordingly. For example:
             * 'Apply iPhone 15 Pro Max' should be returned as a product with brand: 'Apple', product: 'iPhone 15 Pro Max'
@@ -32,10 +21,23 @@ class Analysis::TopicClassification < ApplicationRecord
         Be sure to identify the entity type correctly, as it will be used to determine the next step in the analysis.
     EOF
 
+    include Analysis::InferenceStep
+
+    belongs_to :report
+
+    schema do
+        string :entity_type, enum: %w[brand product service other], description: "The detected category for the input text"
+        string :brand, required: false, description: "If it's a brand, the detected brand for the input text, if it's a product, the brand of or maker of the product"
+        string :product, required: false, description: "If it's a product, the detected product for the input text, minus the brand, that must be included in the brand field"
+        string :other, required: false, description: "If it's not a brand or product, the detected category for the input text"
+    end
+
+    system SYSTEM_PROMPT
+
     def perform
         res = chat("Input: #{self.report.brand_info}")
         unless res.refusal.present?
-            self.topic = normalize(res.parsed)
+            self.topic = self.class.normalize(res.parsed)
         else
             self.error = "Topic classification refused: #{res.refusal}, defaulting to entity_type=other"
             Rails.logger.error(self.error)
@@ -45,7 +47,7 @@ class Analysis::TopicClassification < ApplicationRecord
         true
     end
 
-    def normalize(res)
+    def self.normalize(res)
         _type = res[:entity_type]
         res = case _type
         when "brand"
